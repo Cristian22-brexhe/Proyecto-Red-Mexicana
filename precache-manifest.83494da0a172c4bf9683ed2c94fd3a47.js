1a9127382e31208b798c2187afa4f36eb4181e0e
Joel Ibaceta <mail@joelibaceta.com>
2020-07-24 23:47:39 -0500

    Updates

---
+++ precache-manifest.83494da0a172c4bf9683ed2c94fd3a47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aca69f5a62b1c9ccf6471284beace3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee1120cd26ced6f974f7",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "9b18250140a970b4838d",
+    "url": "/static/css/main.fec45a73.chunk.css"
+  },
+  {
+    "revision": "ee1120cd26ced6f974f7",
+    "url": "/static/js/2.9b2df84d.chunk.js"
+  },
+  {
+    "revision": "82fd2133711d1e027fde858069bc8536",
+    "url": "/static/js/2.9b2df84d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b18250140a970b4838d",
+    "url": "/static/js/main.b9eb2c5c.chunk.js"
+  },
+  {
+    "revision": "b7893f4e0c490fed8154",
+    "url": "/static/js/runtime-main.762b7f25.js"
+  }
+]);
